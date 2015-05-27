644e1a914065bfb8b79b4d26c4d5e083cba2bfeb
Sagarsaurus <sagrsccr@gmail.com>
2015-05-26 21:39:41 -0400

    Fixed geolocation, boy that was easy

--- public/javascripts/util.js
+++ public/javascripts/util.js
@@ -1,6 +1,20 @@
 /**
  * Created by sagarsaurus on 5/26/15.
  */
+function getLocation() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(showPosition);
+    }   else {
+        x.innerHTML = "Geolocation is not supported by this browser.";
+    }
+}
 
+function showPosition(position) {
+    var latitude = position.coords.latitude;
+    var longitude = position.coords.longitude;
+    //console.log(latitude);
+    //console.log(longitude);
+    alert("Latitude is: "+latitude+"\nand Longitude is: "+longitude);
+}
 
 
--- routes/api.js
+++ routes/api.js
@@ -5,18 +5,12 @@
 var express = require('express');
 var router = express.Router();
 var mongoose = require('mongoose');
-var ipify = require('ipify');
-var geo = require('geoip-lite');
 
 var api = {
-    getLocation: function(req, res) {
-        ipify(function (err, ip) {
-            res.status(200).send({message: geo.lookup(ip)});
-        });
-    }
+
 };
 
 
-router.get('/getLocation', api.getLocation);
+//router.get('/getLocation', api.getLocation);
 
 module.exports = router;
