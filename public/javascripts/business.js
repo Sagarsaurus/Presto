55ca8bf558ed6f3d7acdffb9b1106258a9432127
Strongtheory <Deo.Abhishek4@gmail.com>
2015-06-08 03:08:31 -0500

    login page starting point

---
+++ public/javascripts/business.js
@@ -0,0 +1,25 @@
+$('ui.form').form({
+	username: {
+		identifier: 'username',
+		rules: [{
+			type: 'empty',
+			prompt: 'Please enter a username'
+		}]
+	},
+	password: {
+		identifier: 'password',
+		rules: [{
+			type: 'length[6]',
+			prompt: 'Your password must be at least 6 characters'
+		}]
+	},
+	terms: {
+		identifier: 'terms',
+		rules: [{
+			type: 'checked',
+			prompt: 'You must agree to the terms and conditions'
+		}]
+	}
+});
+
+$('.ui.checkbox').checkbox();
