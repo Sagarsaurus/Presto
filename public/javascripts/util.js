edb7d3fce7d6a931c433270940d75e87410068ff
Sagarsaurus <sagrsccr@gmail.com>
2015-06-16 11:53:08 -0400

    Since sunny is working on the page itself, I'm going to clean up the code.

--- public/javascripts/util.js
+++ public/javascripts/util.js
@@ -2,11 +2,6 @@
  * Created by sagarsaurus on 5/26/15.
  */
 var searchOptions = ['accounting', 'airport', 'amusement_park', 'aquarium', 'art_gallery', 'atm', 'bakery','bank', 'bar', 'beauty_salon', 'bicycle_store', 'book_store', 'bowling_alley', 'bus_station', 'cafe', 'campground', 'car_dealer', 'car_rental', 'car_repair', 'car_wash', 'casino', 'cemetary', 'church', 'city_hall', 'clothing_store', 'convenience_store', 'courthouse', 'dentist', 'department_store', 'doctor', 'electrician', 'electronics_store', 'embassy', 'establishment', 'finance', 'fire_station', 'florist', 'food', 'funeral_home', 'furniture_store', 'gas_station', 'general_contractor', 'grocery_or_supermarket', 'gym', 'hair_care', 'hardware_store', 'health', 'hindu_temple', 'home_goods_store', 'hospital', 'insurance_agency', 'jewelry_store', 'laundry', 'lawyer', 'library', 'liquor_store', 'local_government_office', 'locksmith', 'lodging', 'meal_delivery', 'meal_takeaway', 'mosque', 'movie_rental', 'movie_theater', 'moving_company', 'museum', 'night_club', 'painter', 'park', 'parking', 'pet_store', 'pharmacy', 'physiotherapist', 'place_of_worship', 'plumber', 'police', 'post_office', 'real_estate_agency', 'restaurant', 'roofing_contractor', 'rv_park', 'school', 'shoe_store', 'shopping_mall', 'spa', 'stadium', 'storage', 'store', 'subway_station', 'synagogue', 'taxi_stand', 'train_station', 'travel_agency', 'university', 'veterinary_care', 'zoo'];
-var latitude="";
-var longitude="";
-var city="";
-var state="";
-var country="";
 var geocoder;
 function getLocation() {
 
@@ -49,13 +44,13 @@ function handle_errors(error)
         case error.PERMISSION_DENIED: alert("We could not gather your location.  Please make sure location services are enabled in your browser.");
             break;
 
-        case error.POSITION_UNAVAILABLE: alert("Unable to detect current position.");
+        case error.POSITION_UNAVAILABLE: alert("We could not gather your location.  Please make sure location services are enabled in your browser.");
             break;
 
         case error.TIMEOUT: alert("Timed out trying to detect current position.");
             break;
 
-        default: alert("unknown error");
+        default: alert("Unknown Error");
             break;
     }
 }
